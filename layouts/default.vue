923a68a8bfe777ffba65b60d8879ac8f7492ec69
Mohammad Ali Gholizadeh <50132880+moalgholi@users.noreply.github.com>
2024-02-11 16:52:12 +0330

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,17 @@
 <template>
-  <RouterView />
+  <TheNavBar />
+  <TheHeader />
+  <RouterView style="margin-bottom: 5rem" />
 </template>
+
+<style>
+.background {
+  background: rgb(237, 212, 154);
+  background: linear-gradient(
+    160deg,
+    rgba(237, 212, 154, 1) 0%,
+    rgba(240, 227, 198, 1) 29%,
+    rgba(239, 235, 227, 1) 100%
+  );
+}
+</style>
