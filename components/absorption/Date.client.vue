04313f2a2d2bec4d552f2e7d4785fb1cecf92bcb
Mohammad Ali Gholizadeh <50132880+moalgholi@users.noreply.github.com>
2024-02-11 18:42:54 +0330

    Create Date.client.vue

---
+++ components/absorption/Date.client.vue
@@ -0,0 +1,12 @@
+<template>
+  <h3>
+    {{ tarikh.toLocale("fa").format("dddd") }}،
+    {{ tarikh.toLocale("en").format("D") }}
+    {{ tarikh.toLocale("fa").format("MMMM") }}
+    {{ tarikh.toLocale("en").format("YYYY") }}
+  </h3>
+</template>
+
+<script setup>
+var tarikh = new persianDate();
+</script>
