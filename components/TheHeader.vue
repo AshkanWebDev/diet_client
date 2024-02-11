b2df795b506dc86ce1938721e909d15267e940e5
Mohammad Ali Gholizadeh <50132880+moalgholi@users.noreply.github.com>
2024-02-11 16:52:17 +0330

    Create TheHeader.vue

---
+++ components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="logo">
+    <img src="/favicon.png" alt="" />
+    <p>دستیار هوشمند تغذیه</p>
+  </div>
+</template>
+
+<style>
+.logo {
+  user-select: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0.5rem 0;
+}
+.logo img {
+  width: auto;
+  height: 3rem;
+}
+.logo p {
+  font-family: "Blabeloo";
+  margin-right: 1rem;
+}
+</style>
