6348d499a1ac1ea9d550e040235dc828a73ea9bc
Mohammad Ali Gholizadeh <50132880+moalgholi@users.noreply.github.com>
2024-02-12 19:01:32 +0330

    Create Interpretation.vue

---
+++ components/cell/Interpretation.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  props: {
+    height: Number,
+    weight: Number,
+    waist: Number,
+    thigh: Number,
+    perfect_weight: Number,
+    perfect_waist: Number,
+    perfect_thigh: Number,
+  },
+};
+</script>
+
+<template>
+  <ul class="interpretation">
+    <li>تفسیر جدول بالا</li>
+    <li>شما دچار 5 کیلوگرم کمبود وزن هستید.</li>
+
+    <li>شما از ناحیه دور کمر هستید.</li>
+    <li>شما از ناحیه ران هستید.</li>
+  </ul>
+</template>
+
+<style>
+ul.interpretation {
+  margin: 0 1.5rem;
+}
+ul.interpretation li {
+  margin: 0 0.5rem 0.7rem;
+  line-height: 1.3rem;
+}
+ul.interpretation li::before {
+  content: "+";
+  margin-left: 0.3rem;
+}
+ul.interpretation li:first-child {
+  margin: 0 0 1rem;
+}
+ul.interpretation li:first-child::before {
+  display: none;
+}
+</style>
