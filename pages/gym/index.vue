5dcce9ed573c11e49589d681341809643d9cb9e7
Mohammad Ali Gholizadeh <50132880+moalgholi@users.noreply.github.com>
2024-02-12 13:45:35 +0330

    Create index.vue

---
+++ pages/gym/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="gym">
+    <h2>باشگاه</h2>
+    <p>
+      با توجه به جنسیت شما و راحتی استفاده، بخش بانوان از باشگاه شما حذف شده
+      است.
+    </p>
+    <GymLessons />
+  </div>
+</template>
+
+<style>
+.gym {
+  margin: 0 1rem;
+}
+.gym h2 {
+  font-family: "Blabeloo";
+  font-size: var(--h2);
+  text-align: center;
+  margin: 2rem 0 1rem;
+}
+.gym p {
+  line-height: 1.5rem;
+  margin-bottom: 1.5rem;
+}
+</style>
+
+<script setup>
+definePageMeta({
+  // middleware: "auth",
+});
+</script>
