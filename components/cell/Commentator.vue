836788ca71567960f1cd2983e9657f6c603e6804
Mohammad Ali Gholizadeh <50132880+moalgholi@users.noreply.github.com>
2024-02-12 19:01:35 +0330

    Create Commentator.vue

---
+++ components/cell/Commentator.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="commentator">
+    <p>{{ commentator }}</p>
+    <p>{{ result.toFixed(2) }}</p>
+  </div>
+</template>
+
+<style scoped>
+.commentator {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 100px;
+  border-radius: 9px;
+  box-shadow: var(--shadow-1);
+}
+</style>
+
+<script>
+export default {
+  props: ["commentator", "result"],
+};
+</script>
